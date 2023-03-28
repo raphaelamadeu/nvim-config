f428120f6352bf73cfb3e9e9792797c94b9758be
RaphaelAmadeu <raphaelamadeu@pop-os.localdomain>
2023-03-28 13:09:45 -0300

    updated

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>jst", require("jester").run)
+vim.keymap.set("n", "<leader>jft", require("jester").run_file)
+vim.keymap.set("n", "<leader>cp", function()
+  local path = vim.fn.expand("%")
+  vim.fn.setreg("+", path)
+  vim.notify('Copied "' .. path .. '" to the clipboard!')
+end)
